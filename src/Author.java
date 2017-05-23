cd34ba92a0fabc29ce0e45ecdb131f31cc62761e
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-23 14:31:20 -0500

    amended code to fix deprecated and unchecked annotations

--- src/Author.java
+++ src/Author.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
      * @deprecated Use addBook instead
      */
-    @Deprecated
+
     public List<String> getBooks() {
         return books;
     }
@@ -28,7 +28,6 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
